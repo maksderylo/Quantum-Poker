acd8e3f266ad6806173f0787bce2bcfb123e8fa1
maksderylo <129659049+maksderylo@users.noreply.github.com>
2023-10-10 13:31:09 +0200

    Dumbass

--- quantum/src/main/java/Main.java
+++ quantum/src/main/java/Main.java
@@ -1,26 +1,30 @@
-import org.redfx.strange.algorithm.Classic;
 import java.awt.*;
 import javax.swing.*;
+//import java.awt.event.*;
+
 
 public class Main {
 
     void demo() {
         JFrame frame = new JFrame("MF works!", null);
         JButton button = new JButton("ok", null);
+        JLabel label = new JLabel("wadup", null, 0);
 
+        button.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+                        label.setText("Welcome to Javatpoint.");  
+                    }  
+                });
         frame.add(button, BorderLayout.SOUTH);
 
         JPanel newPanel = new JPanel();
         frame.add(newPanel);
-        int bit = Classic.randomBit();
-        String text = "" + bit;
-        JLabel label = new JLabel(text, null, 0);
         frame.add(label, BorderLayout.NORTH);
         Color panelColor = new Color(50, 100, 5);
 
         newPanel.setBackground(panelColor);
 
-        frame.setSize(400, 300);
+        frame.setSize(800, 800);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
