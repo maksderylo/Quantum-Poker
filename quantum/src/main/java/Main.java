fa159798a5fa8b57fbf9c5a7d116d71862ef897b
maksderylo <129659049+maksderylo@users.noreply.github.com>
2023-10-10 14:04:04 +0200

    I'll push

--- quantum/src/main/java/Main.java
+++ quantum/src/main/java/Main.java
@@ -1,28 +1,28 @@
 import java.awt.*;
 import javax.swing.*;
-//import java.awt.event.*;
 
+class BackgroundPanel extends JPanel {
+        final ImageIcon icon = new ImageIcon("mistzombie.jpg");
+        Image img = icon.getImage();
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(img, 0, 0, null);
+        }
+}
 
 public class Main {
 
+    
+
     void demo() {
-        JFrame frame = new JFrame("MF works!", null);
-        JButton button = new JButton("ok", null);
-        JLabel label = new JLabel("wadup", null, 0);
+        
+        JFrame frame = new JFrame("Display an image in the background");
+        BackgroundPanel panel = new BackgroundPanel();
+        frame.add(panel);
 
-        button.addActionListener(new ActionListener(){  
-            public void actionPerformed(ActionEvent e){  
-                        label.setText("Welcome to Javatpoint.");  
-                    }  
-                });
-        frame.add(button, BorderLayout.SOUTH);
 
-        JPanel newPanel = new JPanel();
-        frame.add(newPanel);
-        frame.add(label, BorderLayout.NORTH);
-        Color panelColor = new Color(50, 100, 5);
 
-        newPanel.setBackground(panelColor);
 
         frame.setSize(800, 800);
         frame.setVisible(true);
